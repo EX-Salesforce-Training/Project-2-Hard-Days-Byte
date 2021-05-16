f289bf6c4fdef14d8f7e5de9dc32cdc091daac7c
David J. Sellinger <jonnie@jonniez.com>
2021-05-16 01:04:23 -0400

    Chat finished, controller tested 100%

--- Asteria/force-app/main/default/aura/AsteriaChatTM/AsteriaChatTMController.js
+++ Asteria/force-app/main/default/aura/AsteriaChatTM/AsteriaChatTMController.js
@@ -1,10 +1,6 @@
 ({
-	handleSubmit: function(component, event, helper) {
-		console.log("component.getElements(): ", component.getElements());
-        // access the DOM in c:domLocker
-        let chatboxOld = component.find("chatArea").getElement().innerHTML;
-        console.log(component.find("chatArea").getElement().innerHTML);
-        component.find("chatArea").getElement().innerHTML = chatboxOld + "<tr><td class=\"qTD\">Sample Question</td></tr>" +
-            "<tr><td class=\"aTD\">Sample Answer</td></tr>";
-	}
+    handleSubmit: function(component, event, helper) {
+        component.find("chatArea").getElement().innerHTML = "";
+        helper.getAnswers(component);
+    }
 })
--- Asteria/force-app/main/default/aura/AsteriaChatTM/AsteriaChatTMHelper.js
+++ Asteria/force-app/main/default/aura/AsteriaChatTM/AsteriaChatTMHelper.js
@@ -1,5 +1,26 @@
 ({
-	helperMethod : function() {
-		
+	getAnswers: function(component) {
+		const userInput = component.get("v.userInput");
+        console.log(userInput);
+        
+        let action = component.get("c.getChat");
+        action.setParams({"custQuery":userInput});
+        action.setCallback(this,function(response) {
+            let state = response.getState();
+            console.log(state);
+            if(state === "SUCCESS") {
+                let answers = response.getReturnValue();
+                console.log(answers);
+                let chatboxOld;
+                for (let x of answers) {
+                    let pair = x.split('§');
+                    
+                    chatboxOld = component.find("chatArea").getElement().innerHTML;
+                    component.find("chatArea").getElement().innerHTML = chatboxOld + "<tr><td class=\"qTD\">" + pair[0] + "</td></tr>" +
+                        "<tr><td class=\"aTD\">" + pair[1] + "</td></tr>";
+                }
+            }
+        });
+        $A.enqueueAction(action);
 	}
 })
