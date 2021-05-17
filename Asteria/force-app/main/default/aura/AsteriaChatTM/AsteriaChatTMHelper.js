95e785bcd3c2f30d5d3d6698197d64ab15ce796a
David J. Sellinger <jonnie@jonniez.com>
2021-05-17 12:39:07 -0400

    Everything finished.

--- Asteria/force-app/main/default/aura/AsteriaChatTM/AsteriaChatTMHelper.js
+++ Asteria/force-app/main/default/aura/AsteriaChatTM/AsteriaChatTMHelper.js
@@ -1,5 +1,31 @@
 ({
-	helperMethod : function() {
-		
-	}
+    GetAnswers: function(component) {
+        const userInput = component.get("v.userInput");
+        console.log(userInput);
+        
+        let action = component.get("c.getChat");
+        action.setParams({"custQuery":userInput});
+        action.setCallback(this,function(response) {
+            let state = response.getState();
+            console.log(state);
+            if(state === "SUCCESS") {
+                let answers = response.getReturnValue();
+                console.log(answers);
+                component.set("v.spinner", true);
+                
+                let chatboxOld;
+                setTimeout(function(){
+                    component.set("v.spinner", false);
+                    component.find("chatArea").getElement().innerHTML = "";
+                    for (let x of answers) {
+                        let pair = x.split('§');
+                        
+                        chatboxOld = component.find("chatArea").getElement().innerHTML;
+                        component.find("chatArea").getElement().innerHTML = chatboxOld + "<tr><td class=\"qTD\">" + pair[0] + "</td></tr>" +
+                            "<tr><td class=\"aTD\">" + pair[1] + "</td></tr>";
+                    }},1500);
+            }
+        });
+        $A.enqueueAction(action);
+    }
 })
--- Asteria/force-app/main/default/aura/CreateCase/CreateCaseHelper.js
+++ Asteria/force-app/main/default/aura/CreateCase/CreateCaseHelper.js
@@ -1,15 +1,47 @@
 ({
-	createCase: function(component, newCase) {
-        console.log("in helper.CREATECASE function");
-		let action = component.get("c.saveCase");
-        action.setParams({"newCase":newCase});
+    HandleInit: function(component) {
+        let action = component.get("c.getInitialValues");
         action.setCallback(this, function(response){
             let state = response.getState();
-            console.log(response);
             if (state === "SUCCESS") {
-                console.log("Success! New Case created: " + JSON.stringify(newCase));
+                let valuesMap = response.getReturnValue();
+                component.set("v.userName",valuesMap["UserName"][0]);
+                component.set("v.productValues",valuesMap["Product__c"]);
+                component.set("v.typeValues",valuesMap["Type"]);
+                component.set("v.reasonValues",valuesMap["Reason"]);
             }
         });
         $A.enqueueAction(action);
-	}
+    },
+    HandleSave: function(component) {
+        console.log("in helper.CREATECASE function");
+        let validCase = component.find("caseForm").reduce(function (validSoFar, inputCmp) {
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get("v.validity").valid;
+        }, true);
+        if(validCase) {
+            let action = component.get("c.saveCase");
+            let newCase = component.get("v.newCase");
+            console.log("Create case: " + JSON.stringify(newCase));
+            action.setParams({"newCase":newCase});
+            action.setCallback(this, function(response){
+                let state = response.getState();
+                console.log(response);
+                if (state === "SUCCESS") {
+                    console.log("Success! New Case created: " + JSON.stringify(response.getReturnValue()));
+                    component.set("v.isOpen", false);
+                    component.set("v.newCase", {'sobjectType': 'Case',
+                                                'Origin': 'Web',
+                                                'Product__c': '',
+                                                'Type': '',
+                                                'Reason': '',
+                                                'Subject': '',
+                                                'Description': ''});
+                    let appEvent = $A.get("e.c:UpdateCaseList");
+                    appEvent.fire();
+                }
+            });
+            $A.enqueueAction(action);
+        }
+    }
 })
