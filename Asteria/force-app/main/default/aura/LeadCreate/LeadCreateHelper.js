01980534caf3e5e52211329ce7104fbc4ea6d4f9
Syed Asghar <numair.asghar@gmail.com>
2021-05-17 03:44:33 -0400

    moving updated components in and test class

--- Asteria/force-app/main/default/aura/LeadCreate/LeadCreateHelper.js
+++ Asteria/force-app/main/default/aura/LeadCreate/LeadCreateHelper.js
@@ -7,9 +7,16 @@
                 component.set("v.isShow", false);
                 let leadInsertId = response.getReturnValue();
                 alert('Lead Insert ID: '+ leadInsertId);
+                
+                component.set("v.leadInsert.FirstName", "");
+                component.set("v.leadInsert.LastName", "");
+                component.set("v.leadInsert.Company", "");
+                component.set("v.leadInsert.Email", "");
+                component.set("v.leadInsert.Phone", "");
             }
         })
         $A.enqueueAction(method);
+        
 		
 	}
 })
