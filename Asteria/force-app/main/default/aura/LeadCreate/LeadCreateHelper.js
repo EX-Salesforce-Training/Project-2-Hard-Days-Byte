3cb97033a9254d901a573ba4b528482d1c60a538
Syed Asghar <numair.asghar@gmail.com>
2021-05-17 18:36:09 -0400

    hopefully final push

---
+++ Asteria/force-app/main/default/aura/LeadCreate/LeadCreateHelper.js
@@ -0,0 +1,23 @@
+({
+	save : function(component) {
+        let method = component.get("c.createLeadRecord");
+        method.setParams({"leadInsert":component.get("v.leadInsert")});
+        method.setCallback(this,function(response){
+            if(response.getState()=="SUCCESS"){
+                component.set("v.isShow", false);
+                let leadInsertId = response.getReturnValue();
+                alert('Lead Insert ID: '+ leadInsertId);
+                
+                component.set("v.leadInsert.FirstName", "");
+                component.set("v.leadInsert.LastName", "");
+                component.set("v.leadInsert.Company", "");
+                component.set("v.leadInsert.Email", "");
+                component.set("v.leadInsert.Phone", "");
+                $A.get('e.force:refreshView').fire();
+            }
+        })
+        $A.enqueueAction(method);
+        
+		
+	}
+})
