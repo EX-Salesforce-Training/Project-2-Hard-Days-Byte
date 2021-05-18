8ab9190428ccc092fee08abe342154bfe3a70748
Jmiccolo <jmiccolo89@gmail.com>
2021-05-18 00:42:58 -0400

    Still cleaning up

--- Asteria/force-app/main/default/aura/OpenCases/OpenCasesHelper.js
+++ Asteria/force-app/main/default/aura/OpenCases/OpenCasesHelper.js
@@ -9,4 +9,14 @@
         });
         $A.enqueueAction(action);
 	},
+    FetchAllCases: function(component) {
+		let action = component.get("c.getAllCases");
+        action.setCallback(this, function(response){
+           let state = response.getState(); 
+            if (state === "SUCCESS") {
+                component.set("v.allCases", response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+	},
 })
