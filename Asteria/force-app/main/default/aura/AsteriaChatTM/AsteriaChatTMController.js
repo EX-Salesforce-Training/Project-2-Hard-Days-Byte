792ff9b61f7c4b724ba7f730c6ff0b11a84c1b5d
Jmiccolo <jmiccolo89@gmail.com>
2021-05-18 00:00:34 -0400

    Getting up to date with David

--- Asteria/force-app/main/default/aura/AsteriaChatTM/AsteriaChatTMController.js
+++ Asteria/force-app/main/default/aura/AsteriaChatTM/AsteriaChatTMController.js
@@ -1,10 +1,7 @@
 ({
-	handleSubmit: function(component, event, helper) {
-		console.log("component.getElements(): ", component.getElements());
-        // access the DOM in c:domLocker
-        let chatboxOld = component.find("chatArea").getElement().innerHTML;
-        console.log(component.find("chatArea").getElement().innerHTML);
-        component.find("chatArea").getElement().innerHTML = chatboxOld + "<tr><td class=\"qTD\">Sample Question</td></tr>" +
-            "<tr><td class=\"aTD\">Sample Answer</td></tr>";
-	}
+	HandleSubmit: function(component, event, helper) {
+        component.find("chatArea").getElement().innerHTML = "";
+        helper.GetAnswers(component);
+    }
+    /*future iteration: handle multiple keywords*/
 })
