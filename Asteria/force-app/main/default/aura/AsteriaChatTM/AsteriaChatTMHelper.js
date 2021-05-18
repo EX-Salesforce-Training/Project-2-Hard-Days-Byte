33656c52d0c4a6dad4d00d88efb39a646ddc9dd3
Jmiccolo <jmiccolo89@gmail.com>
2021-05-18 00:23:38 -0400

    Still Cleaning Up

--- Asteria/force-app/main/default/aura/AsteriaChatTM/AsteriaChatTMHelper.js
+++ Asteria/force-app/main/default/aura/AsteriaChatTM/AsteriaChatTMHelper.js
@@ -1,8 +1,8 @@
 ({
-	GetAnswers: function(component) {
+    GetAnswers: function(component) {
         const userInput = component.get("v.userInput");
         console.log(userInput);
-
+        
         let action = component.get("c.getChat");
         action.setParams({"custQuery":userInput});
         action.setCallback(this,function(response) {
@@ -12,14 +12,14 @@
                 let answers = response.getReturnValue();
                 console.log(answers);
                 component.set("v.spinner", true);
-
+                
                 let chatboxOld;
                 setTimeout(function(){
                     component.set("v.spinner", false);
                     component.find("chatArea").getElement().innerHTML = "";
                     for (let x of answers) {
                         let pair = x.split('§');
-
+                        
                         chatboxOld = component.find("chatArea").getElement().innerHTML;
                         component.find("chatArea").getElement().innerHTML = chatboxOld + "<tr><td class=\"qTD\">" + pair[0] + "</td></tr>" +
                             "<tr><td class=\"aTD\">" + pair[1] + "</td></tr>";
